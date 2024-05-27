9ef790c5d45da4d36ae7668486975aa4cfcf0680
andgamespace <anshxc@gmail.com>
2024-05-27 10:39:43 -0500

    _

--- myDir/script.js
+++ myDir/script.js
@@ -1,15 +1,65 @@
+ task = {
+    set_title = "_",
+    description: "_",
+    dueDate: "_",
+    notes: "_",
+    checklist: "_"
+}
 
+function*  taskGenerator(set_title, set_description, set_due_date, set_notes, set_checklist) {
+        
+        object = {
+            title: set_title,
+            description: set_description,
+            dueDate: set_due_date,
+            notes: set_notes,
+            checklist: set_checklist
+            }
+            
+        if(set_description == 0){
+            object.description = "_"
+        } else{
+            object.descritpion = set_description;
+        }
+     
+        if(set_due_date == 0){
+            object.dueDate = "_";
+        } else{
+            object.dueDate = set_due_date;
+        }
 
-function*  taskGenerator(set_title, set_description, set_due_date, set_notes, set_checklist, set_status) {
-    return{
-    title: set_title,
-    description: set_description,
-    dueDate: set_due_date,
-    notes: set_notes,
-    checklist: set_checklist,
-    status: set_status
+        if(set_notes == 0){
+            object.notes = "_";
+        }else{
+            object.notes = set_notes;
+        }
+
+        if(set_checklist == 0){
+            object.checklist = "_";
+        }else{
+            object.checklist = set_checklist;
+        }
+        
+        return object;
     }
 }
+function* projectGenerator(set_title, set_description, set_due_date, set_notes, set_tasks){
+    myProject = {
+        title: set_title,
+        description: set_description,
+        dueDate: set_due_date,
+        notes: set_notes,
+    }
+
+    tasks.forEach(() => {
+        const taskDetails = taskString.split(','); // Splitting each task string by commas to get task properties
+        const task = taskGenerator(taskDetails, 0,0,0,0); // Creating a new Task by spreading the array into the constructor
+        project.addTask(task);
+    });
+
+
+}
+
 function viewAllProjects(){
     do null;
     while(false);
@@ -26,35 +76,10 @@ function deleteTask(){
     do null;
     while(false);
 }
-function markComplete(task){
-    task.status = complete;
-}
 
-var tasks=[
-{
-    title:"",
-    description:"",
-    dueDate:"",
-    notes:"",
-    checklist:""
-},
-{
-    title:"",
-    description:"",
-    dueDate:"",
-    notes:"",
-    checklist:""
-},
-{
-    title:"",
-    description:"",
-    dueDate:"",
-    notes:"",
-    checklist:""
-},
 
-]
+
 function main(){
-    
+
 }
 document.onload(main);
